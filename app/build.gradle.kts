617d20eae884d94d85ed1b670db64981fc6c9d08
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-09-07 21:10:56 +0330

    chore(release): bump version to v0.15.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.goldex.companion"
         minSdk = 24
         targetSdk = 34
-        versionCode = 45
-        versionName = "0.15.0"
+        versionCode = 46
+        versionName = "0.15.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
