05c8bd63639c5c4d59c8e892ecc2423d17277a54
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-09-11 00:04:43 +0330

    fix(customers): import BorderStroke in CustomerDialogs (#82)

--- app/src/main/java/com/goldex/companion/ui/components/CustomerDialogs.kt
+++ app/src/main/java/com/goldex/companion/ui/components/CustomerDialogs.kt
@@ -1,5 +1,6 @@
 package com.goldex.companion.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
