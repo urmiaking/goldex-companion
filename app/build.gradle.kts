99759d74bc2fe5831578ece13ef1255da3c432a4
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-09-11 13:10:40 +0330

    chore: bump version to 0.29.0 (code 63)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.goldex.companion"
         minSdk = 24
         targetSdk = 34
-        versionCode = 62
-        versionName = "0.28.1"
+        versionCode = 63
+        versionName = "0.29.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
